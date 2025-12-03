56d134727b67d8d32f0fbef2ea3112ca21ff1672
2001056 <lbchan1027@gmail.com>
2025-12-03 11:16:07 +0900

    fix

--- git_syntax/2.basic_command.sh
+++ git_syntax/2.basic_command.sh
@@ -1,54 +1,41 @@
 # 현재 working directory, staging area 상태 확인
 git status
 
-# commit 이력 확인 (local repository의 이력)
-git log
-
-# .은 모든 수정/추가 사항 add
+# .은 모든 수정/추가 사항  add
 git add .
 
-# 특정 파일만 add할 경우(경로까지 포함)
+# 특정파일만 add할 경우(경로까지 포함) 
 git add test_folder/test1.txt
 
-# local repository에 반영 및 커밋이력 생성
-git commit -m "메시지 타이틀" -m "메시지 내용"
+# commit을 통해 메시지타이틀과 메시지내용을 커밋이력으로 생성
+git commit -m "메시지제목" -m "메시지내용"
+# git commit만 입력하고 엔터시 vi 모드 -> 첫줄:타이틀, 두번째줄부터 내용 -> :wq 저장후 종료시 커밋ID생성
+git commit
 
-# vi 모드에서 작성 : git commit 후 엔터
-
-# add 와 commit 동시에
-git commit -am "add와 commit 동시에"
-
-# commit 이력 확인(local repository)
+# commit 이력 확인
 git log
-
-# git log 간결하게
 git log --oneline
-# 전체 이력 조회
+
+# head하단의 로그만 보이는것이 아니라, 모든 commit이력을 조회
+# head : 현재 사용(checkout)하고 있는 commitID를 의미
 git log --all
-# 로그를 그래프 형태로 조회
-git log --graph
 
-
-# 원격 저장소로 업로드
+# 원격저장소로 업로드
 git push origin 브랜치명
-# 충돌 발생시 충돌 무시하고, 로컬기준의 코드를 원격에 덮어쓰기(절대 사용 금지)
+# 충돌발생시 충돌무시하고, 로컬기준의 코드를 원격에 덮어쓰기(절대 사용하지 말것)  
 git push origin 브랜치명 --force
 
-
-# 특정 commit id로의 전환
-git checkout 커밋id
+# 특정 commit ID로의 전환
+git checkout 커밋ID
 # 특정 브랜치로의 전환
 git checkout 브랜치명
 
-# pull은 원격사항을 local로 내려받는것 (fetch+merge)
+# pull은 원격사항을 local로 내려받는것(fetch+merge)
 git pull origin main
 
-# fetch는 변경사항을 local로가져오되, 병합은 하지 않는것.
+# fetch는 변경사항을 local가져오되, 병합은 하지 않는것.
 git fetch origin main
 
-
-# commitID간 또는 branch사이의 비교 명령어
+# commitID간 또는 브랜치사이의 비교 명령어
 git diff commitID1 commitID2
-git diff branch1 branch2
-
-
+git diff 브랜치1 브랜치2
